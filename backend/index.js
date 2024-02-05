d7d871535081f39d30ec778c36fee039c5eaa42b
Shubham Jain <shubham@Shubhams-MacBook.local>
2024-02-05 06:10:14 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+//write basic express code with express.json() middleware
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req, res) {
+
+});
+
+app.get("/todos", function(req, res) {
+
+});
+
+app.put("/completed", function(req, res) {
+
+});
